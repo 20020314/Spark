bcbf27df6816633e46a74b95c120392204c89bab
20020314 <yitianyangtwo@qq.com>
2023-02-09 19:57:50 +0800

    Update build.client.sh

--- scripts/build.client.sh
+++ scripts/build.client.sh
@@ -29,25 +29,25 @@ go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/win
 
 
 
-# export GOOS=android
-# export CGO_ENABLED=1
+export GOOS=android
+export CGO_ENABLED=1
 
-# export GOARCH=arm
-# export CC=armv7a-linux-androideabi21-clang
-# export CXX=armv7a-linux-androideabi21-clang++
-# go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_arm Spark/client
+export GOARCH=arm
+export CC=armv7a-linux-androideabi21-clang
+export CXX=armv7a-linux-androideabi21-clang++
+go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_arm Spark/client
 
-# export GOARCH=386
-# export CC=i686-linux-android21-clang
-# export CXX=i686-linux-android21-clang++
-# go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_i386 Spark/client
+export GOARCH=386
+export CC=i686-linux-android21-clang
+export CXX=i686-linux-android21-clang++
+go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_i386 Spark/client
 
-# export GOARCH=arm64
-# export CC=aarch64-linux-android21-clang
-# export CXX=aarch64-linux-android21-clang++
-# go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_arm64 Spark/client
+export GOARCH=arm64
+export CC=aarch64-linux-android21-clang
+export CXX=aarch64-linux-android21-clang++
+go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_arm64 Spark/client
 
-# export GOARCH=amd64
-# export CC=x86_64-linux-android21-clang
-# export CXX=x86_64-linux-android21-clang++
-# go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_amd64 Spark/client
+export GOARCH=amd64
+export CC=x86_64-linux-android21-clang
+export CXX=x86_64-linux-android21-clang++
+go build -ldflags "-s -w -X 'Spark/client/config.COMMIT=$COMMIT'" -o ./built/android_amd64 Spark/client
